b6a25a84722e336ca38f635da34c0715e923a5c2
iamsourav37 <sourav.g29@outlook.com>
2024-03-06 22:19:32 +0530

    now wrost case scenario is implemented

--- Todo/Todo.cs
+++ Todo/Todo.cs
@@ -12,6 +12,12 @@
 
         void SeeTodos()
         {
+            if (this.todos.Count == 0)
+            {
+                Console.WriteLine("No TODOs have been added yet");
+                return;
+            }
+
             for (int i = 0; i < todos.Count; i++)
             {
                 Console.WriteLine($"{i}. {todos[i]}");
@@ -20,23 +26,69 @@
 
         void RemoveTodo()
         {
-            Console.WriteLine("Select the index of the TODO you want to remove:");
-            for (int i = 0; i < todos.Count; i++)
+
+            if (this.todos.Count == 0)
             {
-                Console.WriteLine($"{i}. {todos[i]}");
+                Console.WriteLine("No TODOs have been added yet.");
             }
-            int userChoice = int.Parse(Console.ReadLine());
-            string removedTodo = this.todos[userChoice];
-            this.todos.RemoveAt(userChoice);
-            Console.WriteLine($"TODO removed: [{removedTodo}] ");
+            else
+            {
+                while (true)
+                {
+                    Console.WriteLine("Select the index of the TODO you want to remove:");
+                    for (int i = 0; i < todos.Count; i++)
+                    {
+                        Console.WriteLine($"{i}. {todos[i]}");
+                    }
+                    int userChoice = int.Parse(Console.ReadLine());
+
+                    // Check if user choice is valid or not
+                    if (userChoice < 0 || userChoice > todos.Count)
+                    {
+                        Console.WriteLine("The given index is not valid.");
+                        continue;
+                    }
+                    else
+                    {
+                        string removedTodo = this.todos[userChoice];
+                        this.todos.RemoveAt(userChoice);
+                        Console.WriteLine($"TODO removed: [{removedTodo}] ");
+                        break;
+                    }
+                }
+            }
+
+
         }
 
         void AddTodo()
         {
-            Console.WriteLine("Enter the TODO description: ");
-            string inputTodo = Console.ReadLine();
-            this.todos.Add(inputTodo);
-            Console.WriteLine($"TODO successfully added: [{inputTodo}]");
+
+            while (true)
+            {
+                Console.WriteLine("Enter the TODO description: ");
+                string inputTodo = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(inputTodo))
+                {
+                    // Checking user input is empty or not
+                    Console.WriteLine("The description cannot be empty.");
+                }
+                else if (this.todos.IndexOf(inputTodo) != -1)
+                {
+                    // Duplicate todo
+                    Console.WriteLine("The description must be unique.");
+                }
+                else
+                {
+                    // todo is unique and not empty
+                    // add the todo
+                    this.todos.Add(inputTodo);
+                    Console.WriteLine($"TODO successfully added: [{inputTodo}]");
+                    break;
+                }
+            }
+
         }
 
         public void Run()
@@ -66,15 +118,26 @@
 
         char Menu()
         {
+            bool isSuccessfullyParsed = true;
+            char userChoice;
+            while (true)
+            {
+                Console.WriteLine("Hello!");
+                Console.WriteLine("What do you want to do?");
+                Console.WriteLine("[S]ee all TODOs.");
+                Console.WriteLine("[A]dd a TODO.");
+                Console.WriteLine("[R]emove a TODO.");
+                Console.WriteLine("[E]xit");
+                isSuccessfullyParsed = char.TryParse(Console.ReadLine().ToLower(), out userChoice);
 
-            Console.WriteLine("Hello!");
-            Console.WriteLine("What do you want to do?");
-            Console.WriteLine("[S]ee all TODOs.");
-            Console.WriteLine("[A]dd a TODO.");
-            Console.WriteLine("[R]emove a TODO.");
-            Console.WriteLine("[E]xit");
-
-            return Convert.ToChar(Console.ReadLine().ToLower());
+                if (isSuccessfullyParsed) break;
+                else
+                {
+                    Console.WriteLine("Incorrect input!");
+                    continue;
+                }
+            }
+            return userChoice;
         }
     }
 }
